7e741f356c8a05b8af352266ca75a525b2e21f25
Sean Kermes <skermes@gmail.com>
2017-12-09 08:57:03 -0500

    Solve day nine.

--- src/advent17/nine.clj
+++ src/advent17/nine.clj
@@ -1,8 +1,39 @@
-(ns advent17.nine)
+(ns advent17.nine
+  (:require [clojure.string :as s]))
 
-(defn part-one [] "day nine part one")
+(def example-inputs [
+  "{{<ab>},{<ab>},{<ab>},{<ab>}}"
+  "{{<!!>},{<!!>},{<!!>},{<!!>}}"
+  "{{<a!>},{<a!>},{<a!>},{<ab>}}"
+  "{{<!>},{<!>},{<!>},{<a>}}"
+  "{}"
+  "{{}}"
+  "{{{},{},{{}}}}"])
 
-(defn part-two [] "day nine part two")
+(defn puzzle-input [] (slurp "input/nine-input.txt"))
+
+(defn remove-escapes [text]
+  (s/replace text #"!." ""))
+
+(defn remove-garbage [text]
+  (s/replace text #"<.*?>" ""))
+
+(defn score-groups
+  ([text] (score-groups text 0 0))
+  ([[first-char & rest-chars] depth score]
+    (cond (nil? first-char) score
+          (= \{ first-char) (recur rest-chars (+ depth 1) score)
+          (= \} first-char) (recur rest-chars (- depth 1) (+ score depth))
+          :else (recur rest-chars depth score))))
+
+(defn count-garbage-content [text]
+  (reduce (fn [acc garbage] (+ acc (- (count garbage) 2)))
+          0
+          (re-seq #"<.*?>" text)))
+
+(defn part-one [] (-> (puzzle-input) remove-escapes remove-garbage score-groups))
+
+(defn part-two [] (-> (puzzle-input) remove-escapes count-garbage-content))
 
 (defn day []
   (println (part-one))
