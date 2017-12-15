517c4f963c16d931e50d52542d12c749df1e75ca
Sean Kermes <skermes@gmail.com>
2017-12-15 14:20:50 -0500

    Solve day eleven.

--- src/advent17/eleven.clj
+++ src/advent17/eleven.clj
@@ -1,34 +1,53 @@
 (ns advent17.eleven
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [advent17.shared :as shared]))
 
 (def example-moves ["ne,ne,ne"
                     "ne,ne,sw,sw"
                     "ne,ne,s,s"
                     "se,sw,se,sw,sw"])
 
-(defn parse-moves [text] (s/split text #","))
+(defn puzzle-input [] (slurp "input/eleven.txt"))
 
-(defn hex-move [[x y] direction]
-  (let [y-shift (if (odd? x) 0 -1)]
-    (condp = direction
-           "n" [x (- y 1)]
-           "s" [x (+ y 1)]
-           "ne" [(+ x 1) (+ y y-shift)]
-           "se" [(+ x 1) (+ y 1 y-shift)]
-           "nw" [(- x 1) (+ y y-shift)]
-           "sw" [(- x 1) (+ y 1 y-shift)])))
+(defn parse-moves [text] (s/split (s/trim text) #","))
+
+;; Doing the coordinates kind of wrong here to try to make them fit in the
+;; framework of
+;; http://devmag.org.za/2013/08/31/geometry-with-hex-coordinates/
+
+(def unit-vectors {"n"  [0 1 -1]
+                   "s"  [0 -1 1]
+                   "ne" [1 0 -1]
+                   "se" [1 -1 0]
+                   "nw" [-1 1 0]
+                   "sw" [-1 0 1]})
+
+(defn hex-move [pos direction]
+  (mapv + pos (unit-vectors direction)))
 
 (defn hex-moves [pos directions]
   (reduce hex-move pos directions))
 
-; (defn direction [[from-x from-y] [to-x to-y]]
-;   (cond (and (= from-x to-x) (= from-y to-y)) nil
-;         (= from-x to-x) (if (< from-y to-y) "s" "n")
-;         (< from-x to-x) (if ())))
+(defn all-hex-moves [pos directions]
+  (reductions hex-move pos directions))
 
-(defn part-one [] (map (partial hex-moves [0 0]) (map parse-moves example-moves)))
+(defn abs [n]
+  (max n (- n)))
 
-(defn part-two [] "day eleven part two")
+;; http://devmag.org.za/2013/08/31/geometry-with-hex-coordinates/
+(defn hex-distance-to-origin [[x y z]]
+  (/ (+ (abs x) (abs y) (abs z)) 2))
+
+(defn part-one [] (->> (puzzle-input)
+                       parse-moves
+                       (hex-moves [0 0 0])
+                       hex-distance-to-origin))
+
+(defn part-two [] (->> (puzzle-input)
+                       parse-moves
+                       (all-hex-moves [0 0 0])
+                       (map hex-distance-to-origin)
+                       (apply max)))
 
 (defn day []
   (println (part-one))
